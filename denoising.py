5e6696b44b8555314bcc1ebfb32154f053ef5eb5
cherishdayala <35199567+cherishdayala@users.noreply.github.com>
2021-12-14 23:17:41 -0600

    Updated and added the comments

--- denoising.py
+++ denoising.py
@@ -1,3 +1,4 @@
+# Importing all the required libraries
 import numpy as np
 import pandas as pd
 import os
@@ -13,21 +14,24 @@ from tensorflow.keras.callbacks import EarlyStopping
 import cv2
 from math import log10,sqrt
 
+# Mounting the dataset from the Google Drive
 from google.colab import drive
 drive.mount('/content/gdrive')
 
+# Sorting the dataset images
 train_images=sorted(os.listdir('gdrive/My Drive/Kaggle/CT_COVID/'))
 
+# Training the images
 train_image=[]
 for im in train_images:
   img=image.load_img('gdrive/My Drive/Kaggle/CT_COVID/'+im,target_size=(64,64),color_mode='grayscale')
   img=image.img_to_array(img)
   img=img/255
   train_image.append(img)
-
+# Returing the trained dataset as an numpy array
 train_df=np.array(train_image)
 
-#Subplotting images
+# Plotting images as subplots
 def plot_img(dataset):
   f,ax=plt.subplots(1,5)
   f.set_size_inches(40,20)
@@ -35,7 +39,7 @@ def plot_img(dataset):
     ax[i-5].imshow(dataset[i].reshape(64,64), cmap='gray')
   plt.show()
   
-#Adding gaussian noise with 0.05 factor
+  # Adding Gaussian noise with a factor of 0.05
 def add_noise(image):
   row,col,ch=image.shape
   mean=0
@@ -45,6 +49,7 @@ def add_noise(image):
   noisy=image+gauss*0.05
   return noisy
 
+# Retrieving the Noised Images as an numpy array
 noised_df=[]
 for img in train_df:
   noisy=add_noise(img)
@@ -52,43 +57,51 @@ for img in train_df:
 
 noised_df=np.array(noised_df)
 
+# Plotting the trained dataset images
 plot_img(train_df)
 
+# Plotting the noised images 
 plot_img(noised_df)
 
+# Splitting the data into testing data
 xnoised=noised_df[:300]
 xtest=noised_df[300:]
 
+# Defining Autoencoder
 def autoencoder():
   input_img=Input(shape=(64,64,1),name='image_input')
-  #enoder 
+  # Encoding 
   x = Conv2D(64, (3,3), activation='relu', padding='same', name='Conv1')(input_img)
   x = MaxPooling2D((2,2), padding='same', name='pool1')(x)
   x = Conv2D(64, (3,3), activation='relu', padding='same', name='Conv2')(x)
   x = MaxPooling2D((2,2), padding='same', name='pool2')(x)
    
-  #decoder
+  # Decoding
   x = Conv2D(64, (3,3), activation='relu', padding='same', name='Conv3')(x)
   x = UpSampling2D((2,2), name='upsample1')(x)
   x = Conv2D(64, (3,3), activation='relu', padding='same', name='Conv4')(x)
   x = UpSampling2D((2,2), name='upsample2')(x)
   x = Conv2D(1, (3,3), activation='sigmoid', padding='same', name='Conv5')(x)
    
-  #model
+  # Compiling the Autoencoder Model
   autoencoder = Model(inputs=input_img, outputs=x)
   autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
     
   return autoencoder
 
+# Summarizing the model statistics
 model= autoencoder()
 model.summary()
 
+# Checking the epoch values and fitting the dataset with the trained model
 with tf.device('/device:GPU:0'):
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
     model.fit(xnoised, xnoised, epochs=18, batch_size=10, validation_data=(xtest, xtest), callbacks=[early_stopping])
 
+# Training dataset images
 xtrain= train_df[200:]
 
+# Plotting the images
 pred= model.predict(xtest[:5])
 def plot_predictions(y_true, y_pred):    
     f, ax = plt.subplots(4, 5)
@@ -101,6 +114,7 @@ def plot_predictions(y_true, y_pred):
     plt.tight_layout()
 plot_predictions(xtest[:5], pred[:5])
 
+# Adding Noise to the images using different filters
 median_blur = cv2.medianBlur(xtrain[0], (5))
 gaussian_blur=cv2.GaussianBlur(xtrain[0],(5,5),0)
 average_blur=cv2.blur(xtrain[0],(5,5))
@@ -118,6 +132,7 @@ ax[3].set_title('Average Filter')
 ax[4].imshow(bilateral_filter,cmap='gray')
 ax[4].set_title('Bilateral Filter')
 
+# Calculating the peak signal-to-noise ratio for different filters
 def PSNR(original, denoised): 
     mse = np.mean((original - denoised) ** 2) 
     if(mse == 0): 
@@ -139,4 +154,3 @@ print(f"Median Filter Image : {value1} dB")
 print(f"Gaussian Filter Image : {value3} dB")
 print(f"Average Filter Image : {value4} dB")
 print(f"Bilateral Filter Image : {value5} dB")
-
